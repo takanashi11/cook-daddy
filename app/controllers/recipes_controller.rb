d9179bf54da4092ea4155e581817a405bea95fa1
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-06-09 21:18:03 +0900

    ユーザー管理機能

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,9 @@
 class RecipesController < ApplicationController
   def index
+    @recipe = Recipe.order(created_at: :desc)
+  end
+
+  def new
+    @recipe = Recipe.new
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_09_023517) do
+ActiveRecord::Schema.define(version: 2023_06_09_103606) do
+
+  create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "recipe_title", null: false
+    t.text "recipe_text", null: false
+    t.integer "category_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +41,5 @@ ActiveRecord::Schema.define(version: 2023_06_09_023517) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipes", "users"
 end
