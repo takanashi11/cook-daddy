f24897ee7fc3f002b51e6707191d35ee6642d715
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-06-12 21:24:09 +0900

    投稿、一覧機能

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,25 @@ class RecipesController < ApplicationController
     @recipe = Recipe.order(created_at: :desc)
   end
 
+  def list_display
+    @recipe = Recipe.order(created_at: :desc)
+  end
+
   def new
     @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if  @recipe.save
+       redirect_to root_path
+    else
+     render :new
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:recipe_title,:recipe_material,:recipe_text,:category_id,:price,:image).merge(user_id: current_user.id)
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,10 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  has_one_attached :image
   validates :recipe_title,presence: true
+  validates :recipe_material,presence: true
   validates :recipe_text,presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :price, presence: true,numericality: { greater_than_or_equal_to: 100, less_than_or_equal_to: 9_999_999 }
