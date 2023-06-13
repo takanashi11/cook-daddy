c23e585297e1428674f1c9d7765ff262da93146c
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-06-13 14:13:48 +0900

    詳細機能の実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,9 @@
 class RecipesController < ApplicationController
+  before_action :set_item2, only:[:list_display]
+  before_action :set_item, only:[:show,:move_to_index]
+ # before_action :move_to_index, except: [:index, :show,:new,:create]
+
+
   def index
     @recipe = Recipe.order(created_at: :desc)
   end
@@ -20,8 +25,26 @@ class RecipesController < ApplicationController
     end
   end
 
+  def show
+
+  end
+
   private
   def recipe_params
     params.require(:recipe).permit(:recipe_title,:recipe_material,:recipe_text,:category_id,:price,:image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def set_item2
+    @recipe = Recipe.order(created_at: :desc)
+  end
+
+  def move_to_index
+    if current_user.id != @recipe.user.id 
+      redirect_to root_path
+     end
   end
 end
