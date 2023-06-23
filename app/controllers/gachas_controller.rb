30324579912945a1db33281a4292233123da754b
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-06-23 11:52:57 +0900

    修正

--- app/controllers/gachas_controller.rb
+++ app/controllers/gachas_controller.rb
@@ -2,9 +2,9 @@ class GachasController < ApplicationController
   def index
     category_id = params[:category_id]
     if category_id.present?
-      @recipes = Recipe.where(category_id: category_id).order(Arel.sql('RAND()')).limit(1)
+      @recipes = Recipe.where(category_id: category_id).order("RANDOM()").limit(1)
     else
-      @recipes = Recipe.order(Arel.sql('RAND()')).limit(1)
+      @recipes = Recipe.order("RANDOM()").limit(1)
     end
   end
 end
